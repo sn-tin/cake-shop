1505504161f8b4345a8775ca53131cf9f43d62b8
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2023-02-02 23:11:37 +0800

    remove height and separate  hero content styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <div className={styles.hero}>
             <section className={styles.heroWrapper}>
-                <div>
+                <div className={styles.heroContent}>
                     <h1>Where dedication<br/>& perfection meet.</h1>
                     <p>Where designs meet cakes. Same delivery<br/>around Bacoor, Cavite!</p>
                     <button>Shop Now</button>
