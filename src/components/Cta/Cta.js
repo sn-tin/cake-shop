a5378d32984446eeac0dfac96822dfb74fd3b022
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2023-02-03 00:13:22 +0800

    fix cta alignment

--- src/components/Cta/Cta.js
+++ src/components/Cta/Cta.js
@@ -5,11 +5,13 @@ const Cta = () => {
   return (
     <div className={styles.cta}>
         <section className={styles.ctaWrapper}>
-            <h2>Be the first to know about our<br/>offers & workshops.</h2>
+            <h2>Be the first to know about our offers & workshops.</h2>
+            <div className={styles.formWrapper}>
                 <form className={styles.ctaForm}>
                     <input type="text" name="email" placeholder="Enter your email address"/>
                     <button>Subscribe</button>
                 </form>
+            </div>
         </section>
         <div className={styles.whiteOverlay}></div>
     </div>
