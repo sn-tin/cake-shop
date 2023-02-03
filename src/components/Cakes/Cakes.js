432cd453883882bb3b6eaf9a9a122d5da638d233
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2023-02-03 23:12:47 +0800

    fix cake category styles

--- src/components/Cakes/Cakes.js
+++ src/components/Cakes/Cakes.js
@@ -4,7 +4,7 @@ import data from "../../cakeData"
 import styles from "./Cakes.module.scss"
 
 const CakeList = () => {
-    const cakeCategory = ["All", "Wedding Cake", "Vintage Cake", "Monogram Cake"];
+    const cakeCategory = ["All", "Wedding", "Vintage", "Monogram"];
     const [cakes, setCakes] = useState(data)
     const formatPrice = (value) => value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return (
@@ -14,7 +14,6 @@ const CakeList = () => {
                 <div className={styles.cakeCategory}>
                     { cakeCategory.map(menu => <span key={menu}>{menu}</span>) }
                 </div>
-                <hr/>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 3 }}>
                     { cakes.map(data => (
                         <Grid item xs={6} md={4} mb={5}>
