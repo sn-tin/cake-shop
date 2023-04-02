d5e6cdf7ad334441e02f57a6ef00eac0f83db887
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2023-04-02 23:25:21 +0800

    scroll to top when about page is clicked

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function About() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <div style={{margin: "100px"}}>
       <h2>About Cake It</h2>
