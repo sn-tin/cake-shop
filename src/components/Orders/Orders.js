9a7b804ffb344c0ff168c2a0386e2e59b385dcd8
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2023-04-02 19:40:56 +0800

    add increase and decrease functionality on cart items

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useStateContext } from '../../context/StateContextProvider';
 import styles from '../Orders/Orders.module.scss';
 import EmptyState from './EmptyState';
 
 export default function Orders() {
-  const { cartItems, handleRemoveCart, handleCartClick, totalPrice, cartItemQtyIncrease } = useStateContext()
+  const { cartItems, handleRemoveCart, handleCartClick, totalPrice, cartItemQty, formatPrice } = useStateContext()
+  const plusRef = useRef()
+  const minusRef = useRef()
 
   return (
     <section className={styles.orders}>
@@ -28,12 +30,12 @@ export default function Orders() {
                     <p>{cake.category} Cake</p>
                   </div>
                   <div className={styles.quantity}>
-                    <i className="fa-solid fa-minus fa-xs" ></i>
+                    <i ref={minusRef} className="fa-solid fa-minus fa-xs" onClick={() => cartItemQty(minusRef, cake.index)}></i>
                     <span>{cake.quantity}</span>
-                    <i className="fa-solid fa-plus fa-xs" onClick={cartItemQtyIncrease}></i>
+                    <i ref={plusRef} className="fa-solid fa-plus fa-xs" onClick={() => cartItemQty(plusRef, cake.index)}></i>
                   </div>
                 </div>
-                <p className={styles["order-price"]}>{cake.details.price}.00</p>
+                <p className={styles["order-price"]}>{formatPrice(cake.details.price)}</p>
               </div>
             ))
           )
--- src/context/StateContextProvider.js
+++ src/context/StateContextProvider.js
@@ -44,12 +44,11 @@ export default function StateContextProvider({children}) {
           }
       })
       setCartItems(updatedCartItem)
-      setTotalPrice(prevPrice => prevPrice + product.details.price * quantity)
     }
     
     const addToCart = (product) => {
-      setCartItems([...cartItems, { ...product, quantity }])
-      setTotalPrice(prevPrice => prevPrice + product.details.price * quantity)
+      cartItems.unshift(product)
+      product.quantity = quantity
     }
     
     const onAddClick = (product, button) => {
@@ -62,21 +61,43 @@ export default function StateContextProvider({children}) {
       if(button.toLowerCase() === "buy now"){
         handleCartClick()
       }
+      setTotalPrice(prevTotalPrice => prevTotalPrice + product.details.price * quantity)
       setQuantity(1)
     }
     
     const handleRemoveCart = (cake) => {
       const deleteItem = cartItems.filter(item => item.index !== cake.index)
       setCartItems(deleteItem)
-      setTotalPrice(prevPrice => prevPrice - cake.quantity * cake.details.price)
+      setTotalPrice(prevTotalPrice => prevTotalPrice - cake.quantity * cake.details.price)
     }
 
-    const cartItemQtyIncrease = (product) => {
-      const foundItem = cartItems.find((item) => item.index === product.index);
-      return {
-        ...product,
-        quantity: foundItem.quantity + 1
-      } 
+    const cartItemQty = (ref, id) => {
+      const foundItem = cartItems.find(item => item.index === id)
+      if(ref.current.className.includes("fa-plus")){
+        const updateCartItem = cartItems.map((item) => {
+          if(foundItem.index === id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1
+            }
+          }
+        })
+        setCartItems(updateCartItem)
+        setTotalPrice(prevTotalPrice => prevTotalPrice + parseInt(foundItem.details.price))
+      } else if(ref.current.className.includes("fa-minus")){
+        const updateCartItem = cartItems.map((item) => {
+          if(foundItem.index === id) {
+            return {
+              ...item,
+              quantity: item.quantity - 1
+            }
+          }
+        })
+        console.log(ref)
+        setCartItems(updateCartItem)
+        setTotalPrice(prevTotalPrice => prevTotalPrice - parseInt(foundItem.details.price))
+      }
+      console.log(ref)
     }
     
     const handleCartClick = () => {
@@ -94,7 +115,7 @@ export default function StateContextProvider({children}) {
         onAddClick,
         totalPrice,
         handleRemoveCart,
-        cartItemQtyIncrease,
+        cartItemQty,
         cake,
         displayCakeDetails,
         quantity,
