ed116c02aa7ddd53ef27c0fbd7838915a7cca4b1
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2023-04-07 00:06:43 +0800

    fix style of confirmation purchase pages

--- store/src/pages/Cancel.js
+++ store/src/pages/Cancel.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import styles from "./Confirmation.module.scss";;
 
 function Cancel() {
+  const navigate = useNavigate();
   return (
-    <h1>Sorry to see you cancelled your Stripe payment!</h1>
+    <section className={styles.confirmation}>
+      <h1>Until next time</h1>
+      <p>We're sorry to see you cancelled your Stripe payment. See you on your next purchase!</p>
+      <button onClick={() => navigate('/')}>Continue to Homepage</button>
+    </section>
   )
 }
 
--- store/src/pages/Success.js
+++ store/src/pages/Success.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import styles from "./Confirmation.module.scss";
 
 function Success() {
+  const navigate = useNavigate();
   return (
     <section className={styles.confirmation}>
-      <i className="fa-solid fa-circle-check fa-xl"></i>
-      <h1>THANK YOU<br/><span>FOR YOUR PURCHASE</span></h1>
-      <p>A receipt of orders will be sent to your email.</p>
-      <small>Having trouble? Contact Us</small>
-      <button>Continue to Homepage</button>
+      <h1>Thank you for your purchase!</h1>
+      <p>A receipt of orders will be sent to your email.<br/> It will be delivered 2-5 business days.</p>
+      <p>Email at  <a href="https://mail.google.com/mail/?view=cm&fs=1&href=kristinemaydj.work@gmail.com" target="_blank" rel="noreferrer">cakeit@gmail.com</a> with any questions or suggestions.
+      </p>
+      <br/>
+      <button onClick={() => navigate('/')}>Continue to Homepage</button>
     </section>
   )
 }
